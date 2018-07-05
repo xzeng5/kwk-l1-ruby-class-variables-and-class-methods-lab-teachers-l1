bfd4c64366c916e50692386fa6ab023b7e26b7ac
Anna Zeng <xiange.anna.zeng@gmail.com>
2018-07-05 18:45:59 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,1 +1,46 @@
+class Song
 
+  @@count = 0
+  @@genres = []
+  @@artists = []
+  @@genre_count = {}
+  @@artist_count = {}
+
+  attr_accessor :name, :artist, :genre
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@genres << genre
+    @@artists << artist
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genre_count
+    @@genres.uniq.each do |name|
+      @@genre_count[name] = @@genres.count(name)
+    end
+    @@genre_count
+  end
+
+  def self.artist_count
+    @@artists.uniq.each do |name|
+      @@artist_count[name] = @@artists.count(name)
+    end
+    @@artist_count
+  end
+
+end
